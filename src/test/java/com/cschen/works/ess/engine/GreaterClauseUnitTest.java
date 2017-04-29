7c8f02eeb3ac76b12ae92d1494c58f6b1db6779f
Caishun Chen <cschen1205@gmail.com>
2017-04-29 11:54:05 +0800

    Unit test for the greater than clause

---
+++ src/test/java/com/cschen/works/ess/engine/GreaterClauseUnitTest.java
@@ -0,0 +1,33 @@
+package com.cschen.works.ess.engine;
+
+import com.cschen.works.ess.enums.IntersectionType;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+/**
+ * Created by memeanalytics on 29/4/17.
+ */
+public class GreaterClauseUnitTest {
+
+    private GreaterClause clause = new GreaterClause("temperature", "10");
+
+    @Test
+    public void test_get(){
+        assertThat(clause.getCondition()).isEqualTo(">");
+        assertThat(clause.getVariable()).isEqualTo("temperature");
+        assertThat(clause.getValue()).isEqualTo("10");
+    }
+
+    @Test
+    public void test_insect(){
+        assertThat(clause.intersect(new EqualsClause("temperature", "10"))).isEqualTo(IntersectionType.MutuallyExclusive);
+        assertThat(clause.intersect(new EqualsClause("temperature", "11"))).isEqualTo(IntersectionType.Inclusive);
+        assertThat(clause.intersect(new EqualsClause("temperature", "9.9"))).isEqualTo(IntersectionType.MutuallyExclusive);
+        assertThat(clause.intersect(new GreaterClause("temperature", "9"))).isEqualTo(IntersectionType.Unknown);
+        assertThat(clause.intersect(new GreaterClause("temperature", "11"))).isEqualTo(IntersectionType.Inclusive);
+        assertThat(clause.intersect(new LessClause("temperature", "10"))).isEqualTo(IntersectionType.MutuallyExclusive);
+        assertThat(clause.intersect(new LessClause("temperature", "11"))).isEqualTo(IntersectionType.Unknown);
+    }
+
+}
