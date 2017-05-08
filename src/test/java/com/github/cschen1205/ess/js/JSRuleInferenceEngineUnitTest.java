43b5f7152e7028384c86b831b89dc8ac75d50ec4
Caishun Chen <cschen1205@gmail.com>
2017-05-08 13:58:40 +0800

    unit test for js rule engine load file

--- src/test/java/com/github/cschen1205/ess/js/JSRuleInferenceEngineUnitTest.java
+++ src/test/java/com/github/cschen1205/ess/js/JSRuleInferenceEngineUnitTest.java
@@ -4,6 +4,11 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import javax.script.ScriptException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by cschen on 28/4/17.
@@ -14,14 +19,47 @@ public class JSRuleInferenceEngineUnitTest {
     @BeforeTest
     public void setup() throws ScriptException {
         engine = new JSRuleInferenceEngine();
+
+    }
+
+    @Test
+    public void testLoadFile() throws IOException, ScriptException {
         String jsContent = IOUtil.readToEnd("js/vehicle-rules.js");
-        engine.loadString(jsContent);
+        Files.write(Paths.get("/tmp/rules.js"), Collections.singletonList(jsContent));
+        engine.loadFile("/tmp/rules.js");
+
+        engine.buildRules();
+
+        engine.clearFacts();
+
+        engine.addFact("num_wheels", "4");
+        engine.addFact("motor", "yes");
+        engine.addFact("num_doors", "3");
+        engine.addFact("size", "medium");
+
+
+
+        System.out.println("before inference");
+        System.out.println(engine.getKnowledgeBase());
+        System.out.println();
+
+
+
+        engine.infer(); //forward chain
+
+        System.out.println("after inference");
+        System.out.println(engine.getKnowledgeBase());
+        System.out.println();
+
     }
 
 
 
     @Test
     public void testVehicleScenario() throws ScriptException {
+
+        String jsContent = IOUtil.readToEnd("js/vehicle-rules.js");
+        engine.loadString(jsContent);
 
         engine.buildRules();
 
